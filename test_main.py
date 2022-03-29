7f0a46099082a9297d08805090d6f24859cf1e72
Sania2001 <saniagshinde@gmail.com>
2022-03-29 15:13:04 +0530

    .

--- test_main.py
+++ test_main.py
@@ -1,41 +1,54 @@
 import main
+import pytest
 
-def test_five():
-    a = 10
-    result = main.five(a)
-    assert result == True
 
-def test_five2():
-    a = 9
-    result = main.five(a)
-    assert result == False
+# def input():
+#     a = 25
+#     return a
+@pytest.mark.parametrize("num, output",[(5,True),(2,False),(10,True),(7,False)])
+def test_five(num, output):
+    # a = 10
+    result = main.five(num)
+    assert result == output
 
-def test_seven():
-    b = 49
-    result1 = main.seven(b)
-    assert result1 == True
+@pytest.mark.parametrize("num1, output1",[(5,True),(10,True)])
+def test_five2(num1, output1):
+    # a = 9
+    result = main.five(num1)
+    assert result == output1
 
-def test_seven2():
-    b = 50
-    result1 = main.seven(b)
-    assert result1 == False
+@pytest.mark.parametrize("num, output",[(5,True),(2,False),(10,True),(7,False)])
+def test_seven(num, output):
+    # b = 49
+    result1 = main.seven(num)
+    assert result1 == output
 
-def test_nine():
-    c = 81
-    result2 = main.nine(c)
-    assert result2 == True
+@pytest.mark.parametrize("num, output",[(5,True),(2,False),(10,True),(7,False)])
+def test_seven2(num, output):
+    # b = 50
+    result1 = main.seven(num)
+    assert result1 == output
 
-def test_nine2():
-    c = 82
-    result2 = main.nine(c)
-    assert result2 == False
+@pytest.mark.parametrize("num, output",[(5,True),(2,False),(10,True),(7,False)])
+def test_nine(num, output):
+    #c = 81
+    result2 = main.nine(num)
+    assert result2 == output
 
-def test_eleven():
-    d = 121
-    result3 = main.eleven(d)
-    assert result3 == True
+@pytest.mark.parametrize("num, output",[(5,True),(2,False),(10,True),(7,False)])
+def test_nine2(num, output):
+    #c = 82
+    result2 = main.nine(num)
+    assert result2 == output
 
-def test_eleven2():
-    d = 122
-    result3 = main.eleven(d)
-    assert result3 == False
+@pytest.mark.parametrize("num, output",[(5,True),(2,False),(10,True),(7,False)])
+def test_eleven(num, output):
+    #d = 121
+    result3 = main.eleven(num)
+    assert result3 == output
+
+@pytest.mark.parametrize("num, output",[(11,True),(121,False),(22,True),(33,False)])
+def test_eleven2(num, output):
+    # d = 122
+    result3 = main.eleven(num)
+    assert result3 == output
